13f8bf34797749740334330fc593f184259d79d4
Gregg Tavares <github@greggman.com>
2018-01-11 23:44:38 +0900

    build

--- interval-timer.js
+++ interval-timer.js
@@ -290,7 +290,7 @@ return audio;
 var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
     __webpack_require__(2),
   ], __WEBPACK_AMD_DEFINE_RESULT__ = (function (
-    AudioManager,
+    AudioManager
   ) {
   window.AudioManager = AudioManager;
 }).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),
